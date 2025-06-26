8c77faa1473f07a187ea18484d9b82085e4d8967
kittenwarrior-qb <kittenwarrior2005@gmail.com>
2025-06-26 18:43:18 +0700

    Add jsdoc to analyzing

--- src/analyzing/characterCount.ts
+++ src/analyzing/characterCount.ts
@@ -1,3 +1,14 @@
+/**
+ * Counts the number of characters in a given string.
+ *
+ * @param str - Input string
+ * @returns Total number of characters in the string.
+ * @throws {TypeError} If the input is not a string.
+ *
+ * @example
+ * characterCount("Hello world");  // 11
+ * characterCount("123!@#");       // 6
+ */
 export function characterCount(str: string): number {
     if (typeof str !== "string") {
         throw new TypeError("Input must be a string");
--- src/analyzing/characterFrequency.ts
+++ src/analyzing/characterFrequency.ts
@@ -1,3 +1,15 @@
+/**
+ * Analyzes and returns the frequency of each character in a string.
+ *
+ *
+ * @param str - Input string
+ * @returns An object which keys are characters and values are their frequencies.
+ * @throws {TypeError} If the input is not a string.
+ *
+ * @example
+ * characterFrequency("Hello");   { h: 1, e: 1, l: 2, o: 1 }
+ * characterFrequency("A A B b"); { a: 2, b: 2 }
+ */
 export function characterFrequency(str: string): Record<string, number> {
     if (typeof str !== "string") {
         throw new TypeError("Input must be a string");
