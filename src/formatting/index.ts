8ecad840100271aea6e85e26bfd2606a0a829714
sabha348 <sabha341781@gmail.com>
2025-10-12 20:59:57 +0530

    Add formatDuration function and corresponding tests to format durations into human-readable strings

--- src/formatting/index.ts
+++ src/formatting/index.ts
@@ -1,13 +1,16 @@
 export { capitalize } from './capitalize';
 export { formatNumber } from './number';
 export { formatPhone } from './phone';
+export { formatDuration } from './duration';
 
 import { capitalize } from './capitalize';
 import { formatNumber } from './number';
 import { formatPhone } from './phone';
+import { formatDuration } from './duration';
 
 export const formatting = {
   capitalize,
   formatNumber,
   formatPhone,
+  formatDuration,
 };
