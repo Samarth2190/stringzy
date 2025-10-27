384a6148b9d067ab6452f50f3315c845dad4d3a5
Farkhanda-Dalal <7farkhanda@gmail.com>
2025-10-27 22:53:18 +0530

    Added hexadecimal func and tests

--- src/formatting/index.ts
+++ src/formatting/index.ts
@@ -9,6 +9,7 @@ export { formatFileSize } from './fileSize';
 export { formatOrdinal } from './ordinal';
 export { formatList } from './listToString';
 export { formatTemperature } from './temperature';
+export { formatToHexadecimal } from './hexadecimal';
 
 import { capitalize } from './capitalize';
 import { formatNumber } from './number';
@@ -21,6 +22,7 @@ import { formatFileSize } from './fileSize';
 import { formatOrdinal } from './ordinal';
 import { formatList } from './listToString';
 import { formatTemperature } from './temperature';
+import { formatToHexadecimal } from './hexadecimal';
 
 export const formatting = {
   capitalize,
@@ -33,5 +35,6 @@ export const formatting = {
   formatFileSize,
   formatOrdinal,
   formatList,
-  formatTemperature
+  formatTemperature,
+  formatToHexadecimal
 };
