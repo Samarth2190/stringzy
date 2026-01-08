b8bcc0855b045ab01321e99efc62af2299e67bef
Samarth2190 <samarthruia@gmail.com>
2026-01-08 20:14:43 +0530

    feat: Implementing safe prototyping feature

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,49 @@ export * from './formatting';
 export * from './transformations';
 export * from './validations';
 
-import { analyzing } from './analyzing';
-import { formatting } from './formatting';
-import { transformations } from './transformations';
-import { validations } from './validations';
+import * as analyzing from './analyzing';
+import * as formatting from './formatting';
+import * as transformations from './transformations';
+import * as validations from './validations';
+
+declare global {
+  interface String {
+    capitalize(): string;
+    camelCase(): string;
+    kebabCase(): string;
+    snakeCase(): string;
+    isEmail(): boolean;
+    isPalindrome(): boolean;
+    wordCount(): number;
+    characterCount(): number;
+  }
+}
+
+export function extendStringPrototype(): void {
+  const modules = [analyzing, formatting, transformations, validations];
+
+  modules.forEach((module) => {
+    Object.keys(module).forEach((key) => {
+      const fn = (module as any)[key];
+
+      if (typeof fn === 'function' && !String.prototype.hasOwnProperty(key)) {
+        Object.defineProperty(String.prototype, key, {
+          value: function (this: string, ...args: any[]) {
+            return fn(this, ...args);
+          },
+          writable: true,
+          configurable: true,
+          enumerable: false,
+        });
+      }
+    });
+  });
+}
 
 export default {
   analyzing,
   formatting,
   transformations,
   validations,
+  extendStringPrototype,
 };
